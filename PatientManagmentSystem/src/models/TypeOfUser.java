b75ca6518a634f83cc9f0d27c4af09f10475c953
dhirst1 <dhirst1@DT01201.uopnet.plymouth.ac.uk>
2019-11-21 12:20:45 +0000

    Created the netbeans project and set up the user classes

---
+++ PatientManagmentSystem/src/models/TypeOfUser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author dhirst1
+ */
+public enum TypeOfUser {
+    A,D,P,S
+}
---
+++ PatientManagmentSystem/src/models/User.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author dhirst1
+ */
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected String firstName;
+    protected String surname;
+    protected String address;
+    protected TypeOfUser typeOfUser;
+
+    public User(String username, String password, String firstName, String surname, String address) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public TypeOfUser getTypeOfUser() {
+        return typeOfUser;
+    }
+
+    public void setTypeOfUser(TypeOfUser typeOfUser) {
+        this.typeOfUser = typeOfUser;
+    }
+    
+    
+    
+    
+}
